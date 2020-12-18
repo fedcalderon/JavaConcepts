b369a142c1a322d81c24ef44e468b9f009a7b5ea
fedcalderon <fedcalderon@gmail.com>
2020-12-17 21:38:26 -0500

    Added instance of WorkWithFiles class to test creating an empty file

--- JavaConcepts/src/fundamentals/JavaConcepts.java
+++ JavaConcepts/src/fundamentals/JavaConcepts.java
@@ -5,7 +5,8 @@ public class JavaConcepts {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		System.out.println("Illustrating Java concepts.");
+		WorkWithFiles wf = new WorkWithFiles();
+		wf.createFile("file.txt");
 	}
-
 }
