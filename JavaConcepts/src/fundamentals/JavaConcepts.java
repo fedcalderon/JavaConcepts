0a3b9d29523acc708619bade4eab42a99d3114aa
fedcalderon <fedcalderon@gmail.com>
2021-01-07 19:10:00 -0500

    Added functionality to show how a map works.

--- JavaConcepts/src/fundamentals/JavaConcepts.java
+++ JavaConcepts/src/fundamentals/JavaConcepts.java
@@ -1,8 +1,15 @@
 package fundamentals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
 
 public class JavaConcepts {
 
@@ -15,16 +22,18 @@ public class JavaConcepts {
 		printMsg("Illustrating Java concepts.");
 		// Run code to test file handling with Java
 		//testWorkWithFilesClass();
+		//manageUsers();
 		// Run code to test user interface
 		//testWorkWithSwingClass();
 		
 		testStringSplitter();
 		printMsg("Goodbye!");
+		//testWorkWithSwingClass();
+		//printMsg("Goodbye!");
+		
+		explainMaps();
 	}
 	
-	/**
-	 * Test the swing forms
-	 */
 	public static void testWorkWithSwingClass() {
 		WorkWithSwing wswing = new WorkWithSwing();
 		//wswing.createSimpleFrame();
@@ -46,25 +55,77 @@ public class JavaConcepts {
 		wf.createFile("file.txt");
 		// Update a file with a message passed as an argument
 		wf.updateFile("file.txt", "This is a test to append data to file.");
-		// Create an empty csv file
-		wf.createFile("data.csv");
+
+		// Prompt user for message and append it to a file
+		//wf.promptUserForInputAndUpdateFile("file.txt");
+	}
+	
+	public static void manageUsers() {
+		
+		// Create object to use WorkWithFiles class
+		WorkWithFiles wf = new WorkWithFiles();
+		// User account
 		// Create a StringBuilder object to build csv data
 		StringBuilder dataSB = new StringBuilder();
-		// Create header
-		dataSB.append("Date,Name,Age,City").append("\n");
+		String fname = "data.csv";
+		boolean fileExists = wf.doesFileExit(fname);
+		if (!fileExists) {
+			// Create an empty csv file
+			wf.createFile("data.csv");
+			// Create header
+			dataSB.append("UserID,Date,Name,Age,City,Username,Password").append("\n");
+		}
+
 		// Add data
-		dataSB.append(String.format("%s,%s,%s,%s", new Date(),"Sam","17","Indialantic")).append("\n");
+		Random rand = new Random();
+		int randomNo = (int) (rand.nextDouble() * 10000);
+		dataSB.append(String.format("%s,%s,%s,%s,%s,%s,%s", randomNo, new Date(), "Sam", "17", "Indialantic",
+				"User" + randomNo, "Password1")).append("\n");
+
 		// Update csv file
-		wf.updateFile("data.csv", dataSB.toString());
-		// Prompt user for message and append it to a file
-		wf.promptUserForInputAndUpdateFile("file.txt");
+		wf.updateFile(fname, dataSB.toString());
+	}
+	
+	public static void explainMaps() {
+		//   key      value 
+		Map<Integer, String> userMap = new HashMap<Integer, String>();
+		/*
+		 * A map has a unique key that cannot be repeated
+		 */
+		
+		for(int i=0; i < 10; i++) {
+			Random rand = new Random();
+			int randomNo = (int) (rand.nextDouble() * 10000);
+			userMap.put(randomNo, String.valueOf(randomNo/4));
+		}
+		
+		printMsg(userMap.toString());
+		
+		// Extract info from map
+		for(Entry<Integer, String> entry : userMap.entrySet()) {
+			printMsg("userMap key: " + entry.getKey());
+			printMsg("userMap Value: " + entry.getValue());
+		}
+		
+		List<Integer> list = new ArrayList<Integer>();
+		for(int i=0; i < 10; i++) {
+			Random rand = new Random();
+			int randomNo = (int) (rand.nextDouble() * 10000);
+			list.add(randomNo);
+		}
+		printMsg(list.toString());
+		
+		// Extract data from list
+		for(Integer i : list) {
+			printMsg("list value: " + i.toString());
+		}
 		
 		String hi = "Hello Sam.";
 		hi = hi + " It's a great December day!";
 	}
 	
 	public static void testStringSplitter() {
-		printMsg("Running testStringSplitter()");
+		
 		StringBuilder dataSB = new StringBuilder();
 		// Create header
 		dataSB.append("Date,Name,Age,City").append("\n");
